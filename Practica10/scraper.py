92f96cecd2470ddf4e3dc1905a579c07a96c5c59
maroacc <maroamedina@gmail.com>
2021-11-17 19:35:51 +0100

    Add scraper.py and spider.py

---
+++ Practica10/scraper.py
@@ -0,0 +1,53 @@
+# PRÁCTICA 7 - Beautifulsoup
+# María José Medina Hernández
+
+# Imports
+import requests
+from bs4 import BeautifulSoup
+
+# Get the URLs
+#TODO: poner para q lea las url del otro archivo
+with open(file_path, 'r') as f:
+    reader = csv.reader(f, delimiter = ";")
+    country_matrix = [row for row in reader]
+
+# Download the HTML
+url = "https://en.wikipedia.org/wiki/Comillas_Pontifical_University"
+response = requests.get(url)
+
+# Parse the HTML
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the info card
+# We search by class = "infobox vcard"
+target_tables = soup.find_all("table", class_="infobox vcard")
+
+# Check that there's only one element belonging to the class
+len(target_tables)
+
+# Get the element belonging to the class that we are aiming for
+# (There's only one in this case)
+target_table = target_tables[0]
+
+
+# Data extraction
+
+data = { "seal" : target_table.find("a").find("img")["src"],
+         "motto_latin" : target_table.find_all("i")[0].get_text(),
+         "motto_english" : target_table.find_all("i")[2].get_text(),
+         "motto_spanish" : target_table.find_all("i")[1].get_text(),
+         "type" : tds[2].get_text(),
+         "established" : tds[3].get_text().replace("\xa0", " ").split("(")[0].strip(),
+         "chancellor" : tds[5].get_text(),
+         "vice_chancellor" : tds[6].get_text(),
+         "rector" : tds[7].get_text(),
+         "students" : tds[8].get_text().split("[")[0],
+         "location" : tds[9].get_text(),
+         "campus" : tds[10].get_text(),
+         "colors" : tds[11].get_text(),
+         "affiliations" : tds[4].get_text(),
+         "website" : tds[12].get_text(),
+         "logo" : target_table.find_all("td", class_ = "infobox-full-data")[1].find("img")["src"]
+}
+
+
---
+++ Practica10/spider.py
@@ -0,0 +1,93 @@
+#%%
+
+# PRÁCTICA 9 - Spider
+# María José Medina Hernández
+
+#%%
+
+# Imports
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+# PRIMER NIVEL
+# Obtener las URLs de los países
+
+# Download the countries' HTML
+url = "https://en.wikipedia.org/wiki/Lists_of_universities_and_colleges_by_country"
+response = requests.get(url)
+
+
+# Parse the countries' HTML
+soup = BeautifulSoup(response.content, "html.parser")
+
+
+# Find the lists
+# We start with the universities in Africa (3)
+# 9 in total
+
+# Loop the continents (and subcontinents)
+navigation_list = soup.find_all("ul")
+countries = [] # List of countries
+for i in range(3, 12):
+    countries_list = navigation_list[i].find_all("li")
+    for item in countries_list:
+        countries.append([item.text, item.find("a")["href"]])
+
+
+# Create or open a csv file to write to
+file_path = r'C:\Users\Maria\github\ATD\Practica9\countries.csv'
+with open(file_path, 'w', newline = "") as f:
+    writer = csv.writer(f, delimiter = ";")
+    for row in countries:
+        writer.writerow(row)
+
+
+# SEGUNDO NIVEL
+# Obtener las URLs de las universidades
+
+# Obtain Germany's URLs
+
+
+# Load countries.csv in a matrix
+with open(file_path, 'r') as f:
+    reader = csv.reader(f, delimiter = ";")
+    country_matrix = [row for row in reader]
+
+
+for row in matrix:
+    if row[0] == 'Germany':
+        relative_url = row[1]
+
+
+# Download the HTML of German Univeristies
+# ------IMPORTANT!!--------
+# Normally this part would be located inside of the loop but
+# it is inconvenient in Jupyter Notebooks
+country = "Germany"
+url = f"https://en.wikipedia.org/{relative_url}"
+response = requests.get(url)
+
+# Parse the German Universities' HTML
+soup = BeautifulSoup(response.content, "html.parser")
+
+
+# Loop the continents (and subcontinents)
+navigation_list = soup.find_all("ul")
+universities = [] # List of countries
+for i in range(3, 12):
+    universities_list = navigation_list[i].find_all("li")
+    for item in countries_list:
+        universities.append([country, url, item.text, item.find("a")["href"]])
+
+
+# Create or open a csv file to write to
+file_path = r'C:\Users\Maria\github\ATD\Practica9\universities.csv'
+with open(file_path, 'w', newline = "") as f:
+    writer = csv.writer(f, delimiter = ";")
+    for row in universities:
+        writer.writerow(row)
+
+
+

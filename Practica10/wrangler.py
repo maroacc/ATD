a646c3be7de5787ef0eb0799acdedf9f7f7b19d3
maroacc <maroamedina@gmail.com>
2022-01-09 10:23:28 +0100

    add: data wrangler wrangler.py

--- Practica10/wrangler.py
+++ Practica10/wrangler.py
@@ -1,1 +1,24 @@
 # TODO: verificar que hay las mismas entradas que en json
+
+import json
+
+def load_json(file_path):
+    with open(file_path, "r") as input_file:
+        dic = json.load(input_file)
+        return dic
+
+file_path = r"universities.json"
+universities = load_json(file_path)
+
+keys = []
+for university, data in universities.items():
+    for key in data:
+        keys.append(key)
+
+unique_keys = list(set(keys))
+
+print("----- Unique Keys -----")
+for key in unique_keys:
+    print(key)
+
+            
